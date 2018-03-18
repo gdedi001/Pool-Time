0c850d00d100944d8cb75eacdf8b9ae48dbc64b0
GabrielD <gdedi651@gmail.com>
2018-03-18 02:51:58 -0400

    feat: spawn pool item when raycast collider is triggered

--- Scripts/PlacePoolObjects.cs
+++ Scripts/PlacePoolObjects.cs
@@ -17,6 +17,11 @@ public class PlacePoolObjects : MonoBehaviour {
 	public void TrySpawningItems(Vector2 screenPos){
 		//RayCast from the screen position to see if we hit the water
 		//if do, then create a random pool toy at that position
+		GameObject randPoolItem = PoolItemsPrefab[Random.Range(0, PoolItemsPrefab.Length)];
+		RaycastHit hit;
+		if (Physics.Raycast(ARCamera.ScreenPointToRay(screenPos), out hit)) {
+			Instantiate (randPoolItem, hit.point, Quaternion.identity);
+		}
 	}
 
 }
